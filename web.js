a6e523eb61ca210e02fbb65186bec43f632c9eb8
roaldamundsen <roald.a.gomes@gmail.com>
2013-07-11 07:17:04 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = new Buffer(32);
+var fs = require('fs');
 buffer.write(fs.readFileSync('index.html'))
 
 app.get('/', function(request, response) {
