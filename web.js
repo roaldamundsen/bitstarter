d05af11c0c624ba4da8380f01091984b1e96bbe6
roaldamundsen <roald.a.gomes@gmail.com>
2013-07-11 20:46:55 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var app = express.createServer(express.logger());
 
 var buf = new Buffer(32);
 var fs = require('fs');
-//len = buf.write(fs.readFileSync('index.html', "utf-8"))
-len = buf.write(fs.readFileSync('index.html'))
+len = buf.write(fs.readFileSync('index.html', "utf-8"))
 
 app.get('/', function(request, response) {
   response.send(buf.toString('utf8', 0, len));
